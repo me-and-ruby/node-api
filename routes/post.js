114ca685e75850d855cbfdc6aed5e15668be50f7
scriptingjs.com <nyabutijoash@yahoo.com>
2021-01-31 12:39:29 -0600

    added rest api methods

--- routes/post.js
+++ routes/post.js
@@ -4,5 +4,14 @@ const post_route=express.Router();
 post_route.get("/",(req,res)=>{
   res.send("here all posts")
 })
+post_route.post("/",(req,res)=>{
+  res.send("add a post")
+})
+post_route.get("/:id",(req,res)=>{
+  res.send("get post by id")
+})
+post_route.delete("/",(req,res)=>{
+  res.send("remove a post")
+})
 
 module.exports=post_route;

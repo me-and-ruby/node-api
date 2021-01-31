b715dc30cbf4393491ef47927c18897012b9f276
scriptingjs.com <nyabutijoash@yahoo.com>
2021-01-31 12:06:12 -0600

    added port number

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const express=require('express');
 const bodyparser=require('body-parser');
+
+const app=express();
+const port=3000 ;
+app.use(bodyparser.json());
+
+app.listen(()=>{
+  console.log("Listening on port: "+ port)
+})
